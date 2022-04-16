503d5b764663fe893f1c94decd8dae0725ba4076
devock <ockjungbong@gmail.com>
2022-04-17 00:16:43 +0900

    커스텀 로그인 페이지 생성하기

---
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -0,0 +1,41 @@
+package io.security.corespringsecurity.controller.login;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+	@GetMapping("/login")
+	public String login() {
+		return "user/login/login";
+	}
+	
+	
+//    public String login(@RequestParam(value = "error", required = false) String error,
+//                        @RequestParam(value = "exception", required = false) String exception, Model model){
+//
+//        model.addAttribute("error", error);
+//        model.addAttribute("exception", exception);
+//
+//        return "login";
+//    }
+
+//    @GetMapping("/logout")
+//    public String logout(HttpServletRequest request, HttpServletResponse response){
+//
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//
+//        if(authentication != null){
+//            new SecurityContextLogoutHandler().logout(request,response,authentication);
+//        }
+//        return "redirect:/login";
+//    }
+
+}
