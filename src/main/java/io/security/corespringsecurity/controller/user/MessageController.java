1bbc7845918df1cc45c5d03ef9f9f405b47489d6
devock <ockjungbong@gmail.com>
2022-04-17 21:00:46 +0900

    Ajax 로그인 구현 & CSRF 설정

--- src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
+++ src/main/java/io/security/corespringsecurity/controller/user/MessageController.java
@@ -1,23 +1,23 @@
 package io.security.corespringsecurity.controller.user;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MessageController {
 	
-	@GetMapping(value="/messages")
-	public String mypage() throws Exception {
-
+	@GetMapping(value={"/messages"})
+	public String messages() throws Exception {
 		return "user/messages";
 	}
-	
-	@GetMapping(value="/api/messages")
-	@ResponseBody
-	public String apiMessage() throws Exception {
 
-		return "messages ok";
+	@PostMapping(value={"/api/messages"})
+	@ResponseBody
+	public ResponseEntity apiMessages() throws Exception {
+		return ResponseEntity.ok().body("ok");
 	}
 }
